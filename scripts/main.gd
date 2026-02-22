b102ae1bdfea4b96ce94193f4d189b1543023fe7
Maximilian Scherbaum <maximilian-scherbaum@web.de>
2026-02-22 13:24:12 +0100

    Implemented arcade controlls, game info, improvements to UI and system information

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,143 @@
 extends Node2D
 
 @onready var grid: GridContainer = $Control/ScrollContainer/GridContainer
+@onready var scroll_container: ScrollContainer = $Control/ScrollContainer
+@onready var description_label: RichTextLabel = $Control/DescriptionLabel
+@onready var header_label: Label = $Control/Header
 
 var GameTileScene := preload("res://scenes/game_tile.tscn")
+var default_cover := preload("res://img/cover.png")
+
+const BTN_A     = 0b00000001
+const BTN_B     = 0b00000010
+const BTN_X     = 0b00000100
+const BTN_Y     = 0b00001000
+const BTN_LB    = 0b00010000
+const BTN_RB    = 0b00100000
+const BTN_START = 0b01000000
+
+var controller: Controller
+
+var input_cooldown := 0.3
+var input_timer := 0.0
+var btn_a_was_pressed := false
 
 
 func _ready():
+	controller = Controller.new()
+	controller.start(_get_serial_port())
+
 	load_games()
+
+	var timer = Timer.new()
+	timer.wait_time = 1.0
+	timer.autostart = true
+	timer.timeout.connect(_update_header)
+	add_child(timer)
+	_update_header()
+
+
+func _process(delta: float):
+	if not controller:
+		return
+
+	input_timer -= delta
+
+	var raw_x = controller.get_axis_x()
+	var raw_y = controller.get_axis_y()
+	var buttons = controller.get_buttons()
+
+	if input_timer <= 0:
+		var move_x = (raw_x - 512.0) / 512.0
+		var move_y = (raw_y - 512.0) / 512.0
+
+		if move_x > 0.5:
+			_move_focus(-1)
+			input_timer = input_cooldown
+		elif move_x < -0.5:
+			_move_focus(1)
+			input_timer = input_cooldown
+		elif move_y > 0.5:
+			_move_focus(-grid.columns)
+			input_timer = input_cooldown
+		elif move_y < -0.5:
+			_move_focus(grid.columns)
+			input_timer = input_cooldown
+
+	var btn_a_pressed = (buttons & BTN_A) != 0
+	if btn_a_pressed and not btn_a_was_pressed:
+		_launch_focused_game()
+	btn_a_was_pressed = btn_a_pressed
+
+
+func _move_focus(direction: int):
+	var focused = get_viewport().gui_get_focus_owner()
+	if focused == null:
+		if grid.get_child_count() > 0:
+			grid.get_child(0).get_node("MarginContainer/Thumbnail").grab_focus()
+		return
+
+	var tiles = grid.get_children()
+	for i in range(tiles.size()):
+		var thumb = tiles[i].get_node("MarginContainer/Thumbnail")
+		if thumb == focused:
+			var next_index = clamp(i + direction, 0, tiles.size() - 1)
+			var next_thumb = tiles[next_index].get_node("MarginContainer/Thumbnail")
+			next_thumb.grab_focus()
+			_scroll_to_tile(tiles[next_index])
+			return
+
+
+func _scroll_to_tile(tile: Node):
+	await get_tree().process_frame
+
+	var tile_top = tile.global_position.y - grid.global_position.y
+	var tile_bottom = tile_top + tile.size.y
+
+	var scroll_top = scroll_container.scroll_vertical
+	var scroll_bottom = scroll_top + scroll_container.size.y
+
+	if tile_top < scroll_top:
+		scroll_container.scroll_vertical = int(tile_top)
+	elif tile_bottom > scroll_bottom:
+		scroll_container.scroll_vertical = int(tile_bottom - scroll_container.size.y)
+
+
+func _launch_focused_game():
+	var focused = get_viewport().gui_get_focus_owner()
+	if focused == null:
+		return
+
+	var tile = focused.get_parent().get_parent()
+	if tile.has_meta("game_path") and tile.has_meta("data"):
+		launch_game(tile.get_meta("game_path"), tile.get_meta("data"))
+
+
+func _get_serial_port() -> String:
+	match OS.get_name():
+		"Windows":
+			return "COM3"
+		"Linux", "FreeBSD", "NetBSD", "OpenBSD", "BSD":
+			return "/dev/ttyACM0"
+		"macOS":
+			return "/dev/tty.usbmodem"
+		_:
+			return "/dev/ttyACM0"
+
+
+func _update_header():
+	var datetime = Time.get_datetime_dict_from_system()
+
+	var hour = "%02d" % datetime["hour"]
+	var minute = "%02d" % datetime["minute"]
+	var day = "%02d" % datetime["day"]
+	var month = "%02d" % datetime["month"]
+	var year = str(datetime["year"])
+
+	var time_str = hour + ":" + minute
+	var date_str = day + "." + month + "." + year
+
+	header_label.text = time_str + "  |  " + date_str
 
 
 # --------------------------------------------------
@@ -71,25 +202,25 @@ func add_tile(data: Dictionary, game_path: String):
 	var thumb: TextureButton = tile.get_node("MarginContainer/Thumbnail")
 	var name_label: Label = tile.get_node("Name")
 
-	# Set title
 	name_label.text = data.get("title", "Unknown Game")
 
-	# Load cover image
 	var cover_path = game_path.path_join("cover.png")
 	if FileAccess.file_exists(cover_path):
 		var img = Image.load_from_file(cover_path)
 		if img != null:
 			var tex = ImageTexture.create_from_image(img)
 			thumb.texture_normal = tex
+		else:
+			thumb.texture_normal = default_cover
+	else:
+		thumb.texture_normal = default_cover
 
-	# Store metadata
+
 	tile.set_meta("game_path", game_path)
 	tile.set_meta("data", data)
 
-	# Enable focus for controller/keyboard
 	thumb.focus_mode = Control.FOCUS_ALL
 
-	# Connect press
 	thumb.pressed.connect(func():
 		launch_game(
 			tile.get_meta("game_path"),
@@ -98,7 +229,6 @@ func add_tile(data: Dictionary, game_path: String):
 	)
 
 	var tween: Tween
-	# Always set pivot to center
 	thumb.pivot_offset = thumb.size / 2
 
 	thumb.focus_entered.connect(func():
@@ -106,6 +236,9 @@ func add_tile(data: Dictionary, game_path: String):
 			tween.kill()
 		tween = create_tween()
 		tween.tween_property(thumb, "scale", Vector2(1.12, 1.12), 0.12)
+
+		description_label.bbcode_enabled = true
+		description_label.text = tile.get_meta("data").get("description", "")
 	)
 
 	thumb.focus_exited.connect(func():
@@ -115,7 +248,6 @@ func add_tile(data: Dictionary, game_path: String):
 		tween.tween_property(thumb, "scale", Vector2.ONE, 0.12)
 	)
 
-	# Focus first tile automatically
 	if grid.get_child_count() == 1:
 		thumb.grab_focus()
 
@@ -145,7 +277,6 @@ func launch_game(game_path: String, data: Dictionary):
 
 	OS.execute(full_path, [], [])
 
-	# Quit launcher after starting game
 	get_tree().quit()
 
 
@@ -166,9 +297,7 @@ func shutdown_system():
 # --------------------------------------------------
 func get_games_dir() -> String:
 	if OS.has_feature("editor"):
-		# Running inside the editor
 		return ProjectSettings.globalize_path("res://games")
 	else:
-		# Running exported build
 		var base_dir = OS.get_executable_path().get_base_dir()
 		return base_dir.path_join("games")
